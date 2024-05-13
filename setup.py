c3025b03b5aadf59ba8fe1efda6fbb8b56a5729f
Troepje <troepman@gmail.com>
2024-05-13 19:32:00 +0200

    Make description pypi compatible

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(name='PingDing',
       version='0.0.0',
       description='Graphically show ping result of various nodes',
+      long_description='Graphically show ping result of various nodes',
+      long_description_content_type='text/plain',
       author='Troepje',
       author_email='',
       url='https://github.com/Troepje/PingDing',
